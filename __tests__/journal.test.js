12e72bcf012669cdc9e720d96f1daf3e722dfe74
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-06-22 11:38:08 -0700

    create teaser tests; pass Co-authored-by: mireie <mireie@users.noreply.github.com>

--- __tests__/journal.test.js
+++ __tests__/journal.test.js
@@ -10,11 +10,20 @@ describe('Journal', () => {
   test('it should return a wordcount upon entry', () => {
     const journal = new Journal("title","body"); // create journal first
     expect(journal.wordCount()).toEqual(1);
-  })
-
+  });
+  test('it should return number of vowels from the entry', () => {
+    const journal = new Journal("title","body");
+    expect(journal.vowelCount()).toEqual(1);
+  });
+  test('it should return number of consonants from the entry', () => {
+    const journal = new Journal("title","body");
+    expect(journal.consCount()).toEqual(3);
+  });
+  test('it should return the first 8 words of the body', () => {
+    const journal = new Journal("title","One two three four five six seven eight nine");
+    expect(journal.getTeaser()).toEqual("One two three four five six seven eight");
+  });
 });
-
-
 
 
 // Create a journaling website where a user can write entries including at least a title and body. Create Entry objects that include a method to return the number of words in the entry. Then, add a separate method (or methods) to return the number of vowels and consonants in each entry. Finally, add a method called getTeaser to return the first sentence of the entry. If the sentence is over 8 words, only display those first 8 words.
--- src/journal.js
+++ src/journal.js
@@ -9,3 +9,32 @@ Journal.prototype.wordCount = function () {
   let wordArray = this.body.split(" ");
   return wordArray.length;
 };
+
+Journal.prototype.vowelCount = function () {
+  let vowelArray = ["a","e","i","o","u"];
+  let counter = 0;
+  let bodyArray = this.body.split("");
+  bodyArray.forEach(function(element) { 
+    if (vowelArray.includes(element)) {
+      counter++;
+    }
+  });
+  return counter;
+};
+Journal.prototype.consCount = function () {
+  let vowelArray = ["a","e","i","o","u"];
+  let counter = 0;
+  let bodyArray = this.body.split("");
+  bodyArray.forEach(function(element) { 
+    if (!vowelArray.includes(element)) {
+      counter++;
+    }
+  });
+  return counter;
+};
+Journal.prototype.getTeaser = function () {
+  let teaserArray = [];
+  let bodyArray = this.body.split(" ");
+  teaserArray = bodyArray.slice(0,8);
+  return teaserArray.join(" ");
+}
